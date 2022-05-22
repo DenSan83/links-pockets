0f6f8dd713c37b12ca30027779590b18e66f5244
DenSan83 <dennis.santillan83@gmail.com>
2022-05-22 22:33:27 +0200

    fix moving install fnx to InstallController

--- app/controller/controller.php
+++ app/controller/controller.php
@@ -6,7 +6,8 @@ class Controller {
     {
         // Route '/'
         if (!file_exists('PDO_info.php')) {
-            $this->install();
+            $installController = new InstallController();
+            $installController->install();
             exit;
         }
         if ((new UserController())->countUsers() === 0) {
@@ -18,37 +19,6 @@ class Controller {
 
         $folderController = new FolderController();
         $folderController->fetch($data);
-    }
-
-    public function install()
-    {
-        $settings = [];
-        if (isset($_POST['pdo'])) {
-            $settings['pdo'] = $_POST['pdo'];
-        }
-        if (isset($_POST['confirm'])) {
-            if (isset($_POST['confirm']['no'])) {
-                unset($_POST);
-            } else {
-                // Store DB data
-                $data = [
-                    'host' => $_POST['confirm']['host'],
-                    'name' => $_POST['confirm']['name'],
-                    'user' => $_POST['confirm']['user'],
-                    'pass' => $_POST['confirm']['pass']
-                ];
-                file_put_contents('PDO_info.php', serialize($data));
-
-                // Create tables
-                require_once 'installController.php';
-                $installController = new InstallController();
-                if (!$installController->createTables()) return;
-
-                $this->redirect();
-            }
-        }
-        $view = new View();
-        $view->load('install', $settings);
     }
 
     public function redirect($route = null): void
--- app/controller/installController.php
+++ app/controller/installController.php
@@ -3,6 +3,37 @@ require_once './app/model/installModel.php';
 
 class InstallController extends Controller
 {
+    public function install()
+    {
+        $settings = [];
+        if (isset($_POST['pdo'])) {
+            $settings['pdo'] = $_POST['pdo'];
+        }
+        if (isset($_POST['confirm'])) {
+            if (isset($_POST['confirm']['no'])) {
+                unset($_POST);
+            } else {
+                // Store DB data
+                $data = [
+                    'host' => $_POST['confirm']['host'],
+                    'name' => $_POST['confirm']['name'],
+                    'user' => $_POST['confirm']['user'],
+                    'pass' => $_POST['confirm']['pass']
+                ];
+                file_put_contents('PDO_info.php', serialize($data));
+
+                // Create tables
+                require_once 'installController.php';
+                $installController = new InstallController();
+                if (!$installController->createTables()) return;
+
+                $this->redirect();
+            }
+        }
+        $view = new View();
+        $view->load('install', $settings);
+    }
+    
     public function createTables(): bool
     {
         $allCreated = true;
